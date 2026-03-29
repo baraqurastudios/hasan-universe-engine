72a48ec959ec1bf27965110c8d1e058d8e7555f1
baraqurastudios <alorpathshala1@gmail.com>
2026-03-30 05:11:46 +0600

    Backup: 2026-03-29_23-11-46

---
+++ backups/README.md_2026-03-29_23-11-46.py
@@ -0,0 +1,6 @@
+import os
+
+# এই লাইনটি আপনার কম্পিউটারের মেমরিতে পাসওয়ার্ডটি সেভ করে রাখবে
+os.environ["V8_MASTER_KEY"] = "V8_UNIVERSE_GOD_2026"
+
+print("✅ Master Key successfully saved in System Memory!")
