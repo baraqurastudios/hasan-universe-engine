1bd152203264fbbf6ad450e6c1bda45592b2235f
baraqurastudios <alorpathshala1@gmail.com>
2026-03-30 03:04:53 +0600

    Backup: 2026-03-29_21-04-53

---
+++ backups/README.md_2026-03-29_21-04-53.py
@@ -0,0 +1,6 @@
+import os
+
+# এটি সরাসরি আপনার সিস্টেমে ভেরিয়েবলটি সেভ করে দেবে
+os.environ['V8_MASTER_KEY'] = 'V8_UNIVERSE_GOD_2026'
+
+print("✅ Environment Variable 'V8_MASTER_KEY' set via Python!")
