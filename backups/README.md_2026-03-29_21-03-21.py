0a5bc91a87419f73b6b039582cc881d11798e815
baraqurastudios <alorpathshala1@gmail.com>
2026-03-30 03:03:22 +0600

    Backup: 2026-03-29_21-03-21

---
+++ backups/README.md_2026-03-29_21-03-21.py
@@ -0,0 +1,7 @@
+import os
+
+# পাইথনের মাধ্যমে এনভায়রনমেন্ট ভেরিয়েবল সেট করা
+# এটি আপনার বর্তমান সেশনে মাস্টার কি-টি সেভ করে রাখবে
+os.environ['V8_MASTER_KEY'] = 'V8_UNIVERSE_GOD_2026'
+
+print("✅ Master Key has been set successfully in the Environment!")
