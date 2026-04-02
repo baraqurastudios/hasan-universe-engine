df60c24f3bd1ab996eab31b9c2fa29b1f7c6db61
baraqurastudios <alorpathshala1@gmail.com>
2026-04-02 14:35:24 +0600

    Update guardian.py

--- security/guardian.py
+++ security/guardian.py
@@ -1,16 +1,10 @@
 import os
-import sys
 
 class SecurityManager:
     def __init__(self):
-        # আপনার অরিজিনাল ইমার্জেন্সি ট্রিগার
         self.KILL_TRIGGER = "PROTOCOL_ZERO_V8"
 
-    def validate(self, master_command):
-        # কিল-সুইচ চেক (আপনার আগের লজিক)
-        if master_command == self.KILL_TRIGGER:
-            os._exit(1) # সাথে সাথে সব বন্ধ করে দিবে
-        
-        if not master_command:
-            return False
-        return True
+    def validate(self, command):
+        if command == self.KILL_TRIGGER:
+            os._exit(1)
+        return True if command else False
